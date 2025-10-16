e41351fbf37bfffd52a82e1c1cc5caede2d11f9d
haris818 <harismemon422@gmail.com>
2025-10-16 12:58:10 +0500

    class02

---
+++ class02/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import UserList from './UserList';
+import Examp from './Examp';
+
+const App = () => {
+  return (
+    <>
+      {/* <UserList /> */}
+      <Examp />
+    </>
+  );
+};
+
+export default App;
